f3977edd6a48eaca7d689a4bce424bdfe0bd66ae
Hassan Maudarbocus <Hassanm0301@gmail.com>
2022-05-02 10:21:44 +0400

    converted DLL to single linked list and added deletion of node

--- carsParking.cpp
+++ carsParking.cpp
@@ -14,12 +14,10 @@ void CarsParking::append(string* data){
     if (head == NULL){
         head = new_node;
         new_node -> nextNode = NULL;
-        new_node -> prevNode = NULL;
         return;
     }
 
     new_node->nextNode = head;
-    new_node->prevNode = NULL;
     head = new_node;
     return;
 
@@ -38,3 +36,21 @@ void CarsParking::printList(){
         temp = temp->nextNode;
     };
 };
+
+void CarsParking::del(string plateNum){
+    CarNode* temp = head;
+    if (temp->data[1] != plateNum){
+        while((temp->nextNode)->data[1] != plateNum){
+                temp = temp->nextNode;
+            }
+        if (temp == NULL){
+            cout<<"No matches found for that plate number. \n";
+            return;
+        };
+        temp->nextNode = temp->nextNode->nextNode;
+        return;
+    }
+    head = temp->nextNode;
+    temp->nextNode=NULL;
+    return;
+}
