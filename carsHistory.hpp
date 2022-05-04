56126d0513718ce6810d8dbcd82578d0b72d9864
Hassan Maudarbocus <Hassanm0301@gmail.com>
2022-05-04 16:34:04 +0400

    replaced carHashNode with CarNode to increase efficiency

--- carsHistory.hpp
+++ carsHistory.hpp
@@ -2,28 +2,22 @@
 #define _CARSHISTORY_HPP_
 #include <iostream>
 
-class CarHashNode{
-    public:
-        int header;
-        CarNode* firstNode;
-};
-
 class CarNode{
     public:
         std::string* data;
         CarNode* nextNode;
-        CarNode(std::string* data);
+        CarNode();
 };
 
 class CarsHistory{
     private:
-        CarHashNode** bucket;
+        CarNode** bucket;
         int bucketSize;
         std::string hash(std::string);
     public:
         CarsHistory();
         void insert(std::string* data);
-        void printList();
+        void printDay(std::string);
         int hashToInt(std::string date);
 };
 
