c83839f12eda7d266a2bfdd35c3ffaae33c1a2df
Hassan Maudarbocus <Hassanm0301@gmail.com>
2022-05-04 21:56:49 +0400

    added main menu

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "carsHistory.hpp"
+#include "carsParking.hpp"
+#include <iostream>
+
+int main(){
+    while (true){
+        std::cout<<"###_Automated Parking_####"<<std::endl;
+        std::cout<<"1. Import File"<<std::endl;
+        std::cout<<"2. Add data"<<std::endl;
+        std::cout<<"3. Cars currently in parking"<<std::endl;
+        std::cout<<"4. Remove a vehicle"<<std::endl;
+        std::cout<<"5. History"<<std::endl;
+        std::cout<<"6. Export to file"<<std::endl;
+
+        int res;
+        std::cin>>res;
+    }
+}
