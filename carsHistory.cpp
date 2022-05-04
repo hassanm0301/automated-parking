17eda3f392e4c9ab32334eebd47bf28f6807d868
Hassan Maudarbocus <Hassanm0301@gmail.com>
2022-05-04 18:24:58 +0400

    revised logic to use only one type of node

--- carsHistory.cpp
+++ carsHistory.cpp
@@ -1,47 +1,59 @@
 #include "carsHistory.hpp"
 #include <iostream>
 
+CarNode::CarNode(){
+    this->data = NULL;
+    this->nextNode = NULL;
+}
+
 
 CarsHistory::CarsHistory(){
-    int bucketSize = 0;
-    CarHashNode* bucket = new CarHashNode[30];
+    int bucketSize = 30;
+    CarNode** bucket = new CarNode*[bucketSize];
 };
 
 int CarsHistory::hashToInt(std::string date){
-    int days;
+    int days = 0;
     days += date[0]+date[1]-96;
     days += (date[3]+date[4]-96)*31;
     days += (date[6]+date[7]+date[8]+date[9]-192)*366;
     days -= 2228;
     return days;
-}
+};
 
 void CarsHistory::insert(std::string* data){
     int header = hashToInt(data[0]);
-    for(int i=0; i<bucketSize; i++){
-        CarHashNode* currentHashNode = *(bucket+i);
-        if(header == currentHashNode->header){
-            CarNode* newNode;
-            newNode->data = data;
-            newNode->nextNode = currentHashNode->firstNode;
-            currentHashNode->firstNode = newNode;
-            return;
-        }
-    }
 
-    int bucketMax = sizeof(bucket)/sizeof(bucket[0]);
-    if((bucketSize+1)>(bucketMax*0.9)){
-        CarHashNode** bucketTemp = new CarHashNode*[bucketSize*2];
+    while(header>bucketSize){
+        CarNode** tempBucket = new CarNode*[bucketSize*2];
         for(int i=0; i<bucketSize; i++){
-            bucketTemp[i] = bucket[i];
+            tempBucket[i] = bucket[i];
         }
-        bucket = bucketTemp;
-        bucketSize = bucketSize*2;
+        bucket = tempBucket;
+        delete tempBucket;
+        bucketSize *= 2;
     }
 
-    CarHashNode* newHashNode;
-    newHashNode->firstNode = NULL;
-    newHashNode->header = header;
-    bucket[bucketSize] = newHashNode;
+    CarNode* newNode = new CarNode;
+    newNode->data = data;
+    newNode->nextNode = bucket[header];
+    bucket[header] = newNode;
+    return;
 
 }
+
+void CarsHistory::printDay(std::string day){
+    int header = hashToInt(day);
+    if(bucket[header] == NULL){
+        std::cout<<"Error! No entry found on that date.";
+        return;
+    }
+    CarNode* head = bucket[header];
+    while(head != NULL){
+        for(int i=0; i<4; i++){
+            std::cout<<bucket[header]->data[i]<<", ";
+        }
+        head = head->nextNode;
+        std::cout<<std::endl;
+    }
+};
